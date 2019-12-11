300c159aa5c4bda52333409f0ab60aaa60953567
iTBoyer <itboyer@qq.com>
2019-12-11 20:11:19 +0800

    可执行文件

---
+++ Sources/main.swift
@@ -0,0 +1,8 @@
+// (https://github.com/huos3203)
+//
+
+let fir = CommandLine.arguments[1]
+let sec = CommandLine.arguments[2]
+let sym = CommandLine.arguments[3]
+let cal = Calculator()
+cal.Operation(fir, second: sec, symbol: sym)
